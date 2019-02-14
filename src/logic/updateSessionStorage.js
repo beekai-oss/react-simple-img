2f67b5b929a328205e5ad10a15a9ff2b4b834993
bill <bluebill1049@hotmail.com>
2019-02-14 16:45:35 +1100

    fix flow error

--- src/logic/updateSessionStorage.js
+++ src/logic/updateSessionStorage.js
@@ -1,6 +1,6 @@
 // @flow
 
-export default function updateSessionStorage(src) {
+export default function updateSessionStorage(src: string) {
   const cachedImages = JSON.parse(window.sessionStorage.getItem('__REACT_SIMPLE_IMG__')) || {};
   cachedImages[src] = +new Date();
   window.sessionStorage.setItem('__REACT_SIMPLE_IMG__', JSON.stringify(cachedImages));
