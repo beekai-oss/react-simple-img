2ab78ad059b78c26585fa8b60a9af0a44215008b
bill <bluebill1049@hotmail.com>
2019-02-17 16:26:09 +1100

    update example package.json

--- example/src/index.js
+++ example/src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { initSimpleImg } from 'react-simple-img';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
-initSimpleImg();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
