8f98137d3c4e3f44af37b2f4c34bcf8793e918b9
bill <bluebill1049@hotmail.com>
2019-02-23 09:51:14 +1100

    unit test for initSimpleImg

---
+++ test/logic/initSimpleImg.test.js
@@ -0,0 +1,11 @@
+import initSimpleImg from '../../src/initSimpleImg';
+import observerStart, { defaultConfig } from '../../src/logic/observerStart';
+
+jest.mock('../../src/logic/observerStart');
+
+describe('initSimpleImg', () => {
+  it('should start the observer', () => {
+    initSimpleImg();
+    expect(observerStart).toBeCalledWith(defaultConfig, false);
+  });
+});
