8f5845cb5eee04e2dc94fa67a6a46a90cd112f9c
bill <bluebill1049@hotmail.com>
2019-02-23 13:31:19 +1100

    fix unit test

--- test/utils/applyImage.test.js
+++ test/utils/applyImage.test.js
@@ -42,15 +42,3 @@ describe('applyImage', () => {
     window.__REACT_SIMPLE_IMG__ = undefined;
   });
 });
-
-describe('applyStyle', () => {
-  it('should get target style to opaicty 1', () => {
-    const setAttribute = jest.fn();
-    const target = {
-      getAttribute: () => 'display: none;',
-      setAttribute,
-    };
-    applyStyle(target, 'display: block', true);
-    expect(setAttribute).toBeCalledWith('style', 'display: none;opacity: 1;');
-  });
-});
