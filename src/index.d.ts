b6e233992b7055f4af19de55d255d7727c12f5a7
bluebill1049 <bluebill1049@hotmail.com>
2019-09-13 11:41:42 +1000

    update types

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,43 +1,39 @@
-declare module "react-simple-img" {
-  import * as React from "react";
+import * as React from "react";
 
-  interface State {
-    isDocumentLoad: boolean;
-    isCached: boolean;
-  }
-
-  interface Props {
-    src: string;
-    placeholder?: string | boolean;
-    applyAspectRatio?: boolean;
-    className?: string;
-    width?: number;
-    height?: number;
-    alt?: string;
-    sizes?: string;
-    srcSet?: string;
-    style?: React.CSSProperties;
-    animationDuration?: number;
-    importance?: "low" | "high";
-    onComplete?: VoidFunction;
-    imgStyle?: React.CSSProperties;
-  }
-
-  declare class SimpleImg extends React.Component<Props, State> {
-    public render(): JSX.Element;
-  }
-
-  interface Config {
-    root?: HTMLElement;
-    rootMargin?: string;
-    threshold?: number | Array<number>;
-  }
-
-  declare function initSimpleImg(
-    config?: Config,
-    disableAnimateCachedImg: boolean = false,
-    logConsoleError: boolean = false
-  );
-
-  export { SimpleImg, initSimpleImg };
+export interface State {
+  isDocumentLoad: boolean;
+  isCached: boolean;
 }
+
+export interface Props {
+  src: string;
+  placeholder?: string | boolean;
+  applyAspectRatio?: boolean;
+  className?: string;
+  width?: number;
+  height?: number;
+  alt?: string;
+  sizes?: string;
+  srcSet?: string;
+  style?: React.CSSProperties;
+  animationDuration?: number;
+  importance?: "low" | "high";
+  onComplete?: VoidFunction;
+  imgStyle?: React.CSSProperties;
+}
+
+export interface Config {
+  root?: HTMLElement;
+  rootMargin?: string;
+  threshold?: number | Array<number>;
+}
+
+export class SimpleImg extends React.Component<Props, State> {
+  public render(): JSX.Element;
+}
+
+export function initSimpleImg(
+  config?: Config,
+  disableAnimateCachedImg?: boolean,
+  logConsoleError?: boolean
+): undefined;
